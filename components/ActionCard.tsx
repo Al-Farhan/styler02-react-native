51a7711b7ed56f85295cecd186c0fa48f58f9d11
vsjc-valuers <it@vsjadon.com>
2024-04-20 18:00:54 +0530

    feat:action card complete

--- components/ActionCard.tsx
+++ components/ActionCard.tsx
@@ -30,7 +30,10 @@ export default function ActionCard() {
         </View>
         <View style={styles.footerContainer}>
         <TouchableOpacity onPress={() => openWebsite('https://blog.learncodeonline.in/whats-new-in-javascript-21-es12')}>
-            <Text>Read more</Text>
+            <Text style={styles.socialLinks}>Read more</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => openWebsite('https://blog.learncodeonline.in/whats-new-in-javascript-21-es12')}>
+            <Text style={styles.socialLinks}>Follow us</Text>
         </TouchableOpacity>
         </View>
       </View>
@@ -44,13 +47,51 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     paddingHorizontal: 10,
   },
-  card: {},
-  elevatedCard: {},
-  headingContainer: {},
-  headerText: {},
-  cardImage: {
-    height: 180
+  card: {
+    width: 350,
+    height: 340,
+    borderRadius: 6,
+    marginVertical: 12,
+    marginHorizontal: 16
   },
-  bodyContainer: {},
-  footerContainer: {}
+  elevatedCard: {
+    backgroundColor: '#E07C24',
+    elevation: 3,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: '#333',
+    shadowOpacity: 0.4
+  },
+  headingContainer: {
+    height: 40,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  headerText: {
+    fontSize: 16,
+    fontWeight: '600'
+  },
+  cardImage: {
+    height: 180,
+  },
+  bodyContainer: {
+    padding: 10
+  },
+  footerContainer: {
+    padding: 8,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center'
+  },
+  socialLinks: {
+    fontSize: 16,
+    color: '#000000',
+    backgroundColor: '#FFF',
+    paddingHorizontal: 20,
+    paddingVertical: 6,
+    borderRadius: 6
+  }
 });
